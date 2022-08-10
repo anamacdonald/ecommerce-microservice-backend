4cee2804a4247c253f84885f0382e1db5f886506
Ana MacDonald <ana.macdonald@accenture.com>
2022-08-10 10:46:12 +0200

    build(ecommerce-microservice-backend): create spring initializr project

---
+++ src/main/java/com/digitalab/ecommerce/EcommerceMicroserviceBackendApplication.java
@@ -0,0 +1,13 @@
+package com.digitalab.ecommerce;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcommerceMicroserviceBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EcommerceMicroserviceBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/digitalab/ecommerce/EcommerceMicroserviceBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.digitalab.ecommerce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcommerceMicroserviceBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
